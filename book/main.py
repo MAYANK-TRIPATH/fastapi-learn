7ef3979e1a68a9231363ee2a04fb661f706c6379
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-11-12 16:28:35 +0530

    Req - POST

--- book/main.py
+++ book/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional 
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,13 +8,33 @@ app = FastAPI()
 async def read_root():
     return{"message": "First fastapi" }
 
-
+#path parameters
 @app.get('/greet/{name}')
 async def greet_name(name:str) -> dict:
     return{"message":f"Hey {name}"}
 
 
 #for query parameters
-@app.get('/greet/')
+@app.get('/greet')
 async def query_name(age:str) -> dict:
     return{"message" : f"check" "age"}
+
+#for optional
+@app.get('/greet')
+async def query_name(ame: Optional[str] = 'User', age: int = 0) -> dict:
+    return{"message" : f"Hey {name}", "age" : age}
+
+
+
+#Request body - POST
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+@app.post('/book')
+async def create_book(book_data:BookCreateModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+    }
