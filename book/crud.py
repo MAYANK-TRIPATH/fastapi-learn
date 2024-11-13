722c7091ddc2b09d020b060e84d175e4fb978690
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-11-13 14:20:23 +0530

    Delete

--- book/crud.py
+++ book/crud.py
@@ -118,3 +118,16 @@ async def update_book(book_id: int, update_book_data: BookUpdateModel) -> dict:
                 return book
           
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not patch found")
+
+
+# Delete 
+
+@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+      for book in books:
+            if book[id] == book_id:
+                  books.remove(book)
+
+                  return {}
+
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Deleted not happened") 
