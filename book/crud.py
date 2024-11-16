308972471e3c08a9470cbaa6c8e9fb8f0f90fd7b
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-11-17 01:21:34 +0530

    Revision coding

--- book/crud.py
+++ book/crud.py
@@ -79,62 +79,102 @@ class BookUpdateModel(BaseModel):
         language: str        
 
 
+# @app.get('/books', response_model=List[Book])
+# async def get_all_books():
+#     return books
+
+
+# # POST data in DB
+# @app.post('/books', status_code=status.HTTP_201_CREATED)
+# async def create_a_book(book_data:Book) -> dict:
+#       new_book = book_data.model_dump()
+
+#       books.append(new_book)
+
+#       return new_book
+
+# #logic in GET request
+
+# @app.get('/book/{book_id}',)
+# async def get_book(book_id:int) -> dict:
+#     for book in books:
+#           if book["id"] == book_id:
+#                 return book
+#           raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not founnd")
+
+
+# # Update DB -> PATCH
+
+# @app.patch('book/{}')
+# async def update_book(book_id: int, update_book_data: BookUpdateModel) -> dict:
+
+#     for book in books:
+#           if book['id'] == book_id:
+#                 book['title'] == update_book_data.title
+#                 book['publisher'] == update_book_data.publisher
+#                 book['page_count'] == update_book_data.page_count
+#                 book['language'] == update_book_data.language
+
+#                 return book
+          
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not patch found")
+
+
+# # Delete 
+
+# @app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_book(book_id: int):
+#       for book in books:
+#             if book[id] == book_id:
+#                   books.remove(book)
+
+#                   return {}
+
+#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Deleted not happened") 
+
+
+
+#Revision code
+
+# GET req
+
 @app.get('/books', response_model=List[Book])
 async def get_all_books():
     return books
 
 
-# POST data in DB
-@app.post('/books', status_code=status.HTTP_201_CREATED)
-async def create_a_book(book_data:Book) -> dict:
-      new_book = book_data.model_dump()
+# POST req
+@app.post('/books', status_code= status.HTTP_201_CREATED)
+async def create_a_book():
+    new_book = book_data.model_dump()
 
-      books.append(new_book)
+    books.append(new_book)
 
-      return new_book
+    return new_book
 
-#logic in GET request
+# Get req with logics
 
-@app.get('/book/{book_id}',)
-async def get_book(book_id:int) -> dict:
+@app.get('book/{books_id}')
+async def get_book(book_id: int) -> dict:
     for book in books:
-          if book["id"] == book_id:
-                return book
-          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not founnd")
+        if book["id"] == book_id:
+            return book
+
+raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
 
 
-# Update DB -> PATCH
+# Patch req
 
-@app.patch('book/{}')
-async def update_book(book_id: int, update_book_data: BookUpdateModel) -> dict:
+@book_router.patch("/book/{book_id}")
+async def add_book(book_id: int, book_update_data:BookUpdateModel) -> dict:
 
     for book in books:
-          if book['id'] == book_id:
-                book['title'] == update_book_data.title
-                book['publisher'] == update_book_data.publisher
-                book['page_count'] == update_book_data.page_count
-                book['language'] == update_book_data.language
+        if book["id"] == book_id:
+            book["title"] == book_update_data.title
+            book["publisher"] == book_update_data.publisher
 
-                return book
-          
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not patch found")
+            return book
+
+raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not bataunga")
 
 
-# Delete 
-
-@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id: int):
-      for book in books:
-            if book[id] == book_id:
-                  books.remove(book)
-
-                  return {}
-
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Deleted not happened") 
-
-
-
-
-            #Revision code
-
-            
